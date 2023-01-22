8bbb0b81ecb05cfe5d473012da9591269ee41bd8
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-01-22 22:29:10 +0530

    Add Part time Employee & Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,16 +1,18 @@
 echo "Welcome to Employee Wage Computation Program"
 
-isPresent=1;
-randomCheck=$((RANDOM%2));
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
 
-if [ $isPresent -eq $randomCheck ];
+if [ $isFullTime -eq $randomCheck ]
 then
-echo "Employee is Present*";
-empRatePerHr=20
-empHrs=8;
-salary=$(($empHrs*$empRatePerHr))
-echo "salary="$salary
+empHrs=8
+elif [ $isPartTime -eq $randomCheck ]
+then
+empHrs=4
 else
-echo "Employee is Abscent*";
-echo "salary="0
+empHrs=0
 fi
+salary=$(($empHrs*$empRatePerHr));
+echo $salary 
